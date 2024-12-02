eb3979611f66247467e8500b63edbd9fa3dded8f
doDomoGu <kaka_71936410@qq.com>
2024-12-02 22:12:00 +0800

    中间件 myAuth

--- packages/frontend/server/node/src/middlewares/auth.js
+++ packages/frontend/server/node/src/middlewares/auth.js
@@ -3,14 +3,19 @@ const DB = require('@nana/db-mysql_node')
 const RESPONSE = require('@frontServer/utils/response')
 
 module.exports = async function (req, res, next) {
+  const unauthPaths = ['/user/login', '/user/register']
+  if (unauthPaths.includes(req.path)) {
+    return next()
+  }
+
   const authHeader = req.headers['authorization'] || '';
   const token = authHeader.split('Bearer ')[1];
 
   const result = await DB.login_session.verify({ token })
   if (result.code == 200 && result.data != null) {
-    next()
+    return next()
   } else {
-    res.status(401).send(RESPONSE.error('no permission'))
+    return res.status(401).send(RESPONSE.error('no permission', 401))
   }
 
 }
